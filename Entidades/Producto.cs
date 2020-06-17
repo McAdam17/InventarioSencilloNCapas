cb8e72afefca800b7818e7edc0126da9dc20d633
Adan Mota Cruz <adanmotacruz17@gmail.com>
2020-06-17 15:41:23 -0500

    Proyectos base creados y definición de las clases de Entidades

---
+++ Entidades/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public decimal precio { get; set; }
+        public int categoria { get; set; }
+        public int stock { get; set; }
+        public int unidad { get; set; }
+    }
+}
